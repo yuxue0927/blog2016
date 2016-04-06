b488cd5b1544a6604b1be31063a2bd0bcc0590d8
yuxue0927 <237586390@qq.com>
2016-04-06 20:06:55 +0800

    manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    sys.path.append('/storage/sdcard0/sl4a/package/')
+    sys.path.append('/storage/sdcard0/sl4a/site_packages/')
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
     from django.core.management import execute_from_command_line
